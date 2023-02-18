6372da96d503244a7e0ac55645fe17cb6f130cd0
Ben Williams <git@biwills.com>
2023-02-17 16:13:10 -0800

    added `envTypes.ts` and `env.ts` for testing

---
+++ env.ts
@@ -0,0 +1,7 @@
+import type { ENVTypes } from './envTypes'
+
+export const envVars: ENVTypes = {
+  app: {
+    NEXT_PUBLIC_BOUNTY_PROGRAM_ID: 'hello-world',
+  },
+}
---
+++ envTypes.ts
@@ -0,0 +1,8 @@
+// used for testing
+
+export type ENVTypes = {
+  app: {
+    [key: string]: string // allow for extra keys
+    NEXT_PUBLIC_BOUNTY_PROGRAM_ID: string
+  }
+}
